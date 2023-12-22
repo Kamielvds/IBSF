d8777a18ab103c6a3e17590d2b84bfc5a2c063ba
Kamiel Van der Straeten <kamielvdstraeten@icloud.com>
2023-12-22 17:37:00 +0100

    improved syntax

--- Program/Program.cs
+++ Program/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections;
+using DataProcessing.commands;
+using static DataProcessing.commands.FilterCmd;
 
 namespace DataProcessing
 {
@@ -11,15 +14,23 @@ namespace DataProcessing
 
         private static bool RequestUserInput()
         {
-            var userInput = Console.ReadLine();
-            switch (userInput)
+            var userInput = Console.ReadLine()?.Split(' ');
+            if (userInput == null) return true;
+            switch (userInput[0])
             {
+                // filter
+                case "filter":
                 case "-f":
-                    
+                    FilterCommand(userInput);
+                    break;
                 case "q":
                 case "quit":
                     return false;
+                default: 
+                    Console.WriteLine("Command not found.");
+                    break;
             }
+
             return true;
         }
     }
