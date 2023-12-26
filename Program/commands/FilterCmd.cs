6145fcd94b7d433fae16b806171c69b6efc297dd
Kamiel Van der Straeten <kamielvdstraeten@icloud.com>
2023-12-26 13:08:44 -0600

    wrote the filtering inside of the switch statement instead of calling a separate function

--- Program/commands/FilterCmd.cs
+++ Program/commands/FilterCmd.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using Filtering;
 using static DataProcessing.Program;
 using static Filtering.Filters;
@@ -8,37 +9,31 @@ namespace DataProcessing.commands
 {
     public static class FilterCmd
     {
+        public static List<int> filtred;
+
         public static void FilterCommand(string[] command)
         {
             switch (command[1])
             {
                 case "gender":
                 case "--g":
-                    FilterByGender(command);
+                    char targetGender;
+                    try
+                    {
+                        targetGender = Convert.ToChar(command[2]);
+                    }
+                    catch
+                    {
+                        Console.WriteLine("Gender should be a char.");
+                        break;
+                    }
+
+                    filtred = IndexFilterList(Gender, targetGender);
+                    break;
+                default:
+                    Console.WriteLine("No valid command was given");
                     break;
             }
-        }
-
-        private static void FilterByGender(string[] command)
-        {
-            char targetGender;
-            try
-            {
-                targetGender = Convert.ToChar(command[2]);
-            }
-            catch
-            {
-                Console.WriteLine("Genders are supposed to be Char's");
-                return;
-            }
-            
-            var filteredList = FilterList(Gender,targetGender);
-
-            foreach (var gender in filteredList)
-            {
-                Console.WriteLine(gender);
-            }
-            
         }
     }
 }
