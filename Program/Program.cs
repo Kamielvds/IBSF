8695a686f5897121edfa3fabde6b61ac4a10546f
Kamiel Van der Straeten <kamielvdstraeten@icloud.com>
2023-12-26 18:10:37 -0600

    Started initialization and Checking

--- Program/Program.cs
+++ Program/Program.cs
@@ -6,13 +6,30 @@ namespace DataProcessing
 {
     internal class Program
     {
-        public static List<char> Gender = new List<char>{ 'm', 'v', 'm', 'm', 'v', 'v', 'v', 'm', 'm' };
+        public static List<char> Gender = new List<char>
+        {
+            'm',
+            'v',
+            'm',
+            'm',
+            'v',
+            'v',
+            'v',
+            'm',
+            'm'
+        };
+
+        public static List<string> Name = new List<string> { "piet", "jane", "piet", "piet" };
+        public static List<string> Time = new List<string> { "piet", "jane", "piet", "piet" };
 
         public static void Main(string[] args)
         {
-            while (RequestUserInput())
-            {
-            }
+            if (CheckIntregrity())
+                while (RequestUserInput())
+                {
+                }
+
+            Console.WriteLine("The given data is invalid");
         }
 
         private static bool RequestUserInput()
@@ -34,6 +51,19 @@ namespace DataProcessing
                     break;
             }
 
+            return true;
+        }
+
+        private static void Initialize(string givenPath)
+        {
+        }
+
+        private static bool CheckIntregrity()
+        {
+            var tSize = Gender.Count;
+            if (Name.Count != tSize) return false;
+            if (Time.Count != tSize) return false;
+            
             return true;
         }
     }
