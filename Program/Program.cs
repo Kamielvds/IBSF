63ddd593fc99d4308947ab9bc20b326832774d8a
Kamiel Van der Straeten <kamielvdstraeten@icloud.com>
2023-12-28 19:42:20 -0600

    Added age List

--- Program/Program.cs
+++ Program/Program.cs
@@ -6,10 +6,11 @@ namespace DataProcessing
 {
     internal class Program
     {
-        public static List<string> Name = new List<string>();
-        public static List<char> Gender = new List<char> ();
-        public static List<double> Time = new List<double>();
-        public static List<string> Nationality = new List<string>();
+        public static List <string>  Name         = new List<string>();
+        public static List <char>    Gender       = new List<char> ();
+        public static List <double>  Time         = new List<double>();
+        public static List <string>  Nationality  = new List<string>();
+        public static List <int>     Age          = new List<int>();
 
         public static void Main(string[] args)
         {
