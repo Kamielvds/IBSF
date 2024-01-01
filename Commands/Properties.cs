547efadc5d33e18fc9207d354b70090d88497b8e
Kamiel Van der Straeten <kamielvdstraeten@icloud.com>
2024-01-01 18:26:19 +0100

    Added Properties.cs

---
+++ Commands/Properties.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace Commands
+{
+    public class Properties
+    {
+        public static List<string> Name = new List<string>();
+        public static List<char> Gender = new List<char>();
+        public static List<double> Time = new List<double>();
+        public static List<string> Nationality = new List<string>();
+        public static List<int> Age = new List<int>();
+    }
+}
