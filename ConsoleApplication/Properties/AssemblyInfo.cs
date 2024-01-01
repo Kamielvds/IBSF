06bac1464c2159bdf5e1f8901c8ed3e652d4b1a9
Kamiel Van der Straeten <kamielvdstraeten@icloud.com>
2024-01-01 18:51:45 +0100

    Cleaned up unused files and directories

--- ConsoleApplication/Properties/AssemblyInfo.cs
+++ ConsoleApplication/Properties/AssemblyInfo.cs
@@ -4,11 +4,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ConsoleApp")]
+[assembly: AssemblyTitle("Program")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ConsoleApp")]
+[assembly: AssemblyProduct("Program")]
 [assembly: AssemblyCopyright("Copyright ©  2023")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -19,7 +19,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("D42DDD63-90BF-4D50-914E-F104473CA3C5")]
+[assembly: Guid("114A3623-1CF9-4F70-9B25-241F6E180E34")]
 
 // Version information for an assembly consists of the following four values:
 //
