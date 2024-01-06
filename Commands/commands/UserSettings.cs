d8ff456e7a44b5455de3cf5f9dd634b33ea0b960
Kamiel Van der Straeten <kamielvdstraeten@icloud.com>
2024-01-06 18:39:27 +0100

    changed and added includes, as well as remove non-functional code

--- Commands/commands/UserSettings.cs
+++ Commands/commands/UserSettings.cs
@@ -1,6 +1,7 @@
 using System;
-using System.Xml.Serialization;
+using System.Linq;
 using System.Xml;
+using System.Xml.Linq;
 
 namespace DataProcessing.commands
 
@@ -20,21 +21,7 @@ namespace DataProcessing.commands
 
         private static void UpdatePath(string xmlContent)
         {
-            var xmlDoc = new XmlDocument();
-            xmlDoc.Load("UserSettings/UserSettings.xml"); 
-
-            var rootElement = xmlDoc.SelectSingleNode("/path");
-
-            if (rootElement != null)
-            {
-                rootElement.InnerText = xmlContent;
-                xmlDoc.Save("UserSettings/UserSettings.xml"); 
-                Console.WriteLine("Path saved successfully");
-            }
-            else
-            {
-                Console.WriteLine("Root element 'path' not found, check xml file.");
-            }
+            
         }
     }
 }
