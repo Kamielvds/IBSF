114bd4bac6db733283c735374cf2b3e14bd3319e
Kamiel Van der Straeten <kamielvdstraeten@icloud.com>
2024-01-14 20:25:36 +0100

    Updated properties.cs for OO use

--- Commands/Properties.cs
+++ Commands/Properties.cs
@@ -4,6 +4,22 @@ namespace Commands
 {
     public class Properties
     {
-        public static Dictionary<string, List<Dictionary<string, object>>> UserScores = new Dictionary<string, List<Dictionary<string, object>>>();
+        public Properties(string filePath, string lang)
+        {
+            _filePath = filePath;
+            _lang = lang;
+        }
+
+        private string _filePath;
+        private string _lang;
+        
+        public string FilePath
+        {
+            get => _filePath;
+            set => _filePath = value;
+        }
+        
+        public static Dictionary<string, List<Dictionary<string, object>>> UserScores 
+            = new Dictionary<string, List<Dictionary<string, object>>>();
     }
 }
