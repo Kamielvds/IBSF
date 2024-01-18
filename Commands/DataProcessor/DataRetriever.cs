4baa39785f5a2357c6589f723aed5ff446b0080b
Kamiel Van der Straeten <kamielvdstraeten@icloud.com>
2024-01-18 13:25:39 +0100

    Changed accessibility of properties

--- Commands/DataProcessor/DataRetriever.cs
+++ Commands/DataProcessor/DataRetriever.cs
@@ -1,10 +1,25 @@
+using System.Collections.Generic;
+using System.Linq;
+
 namespace Commands.DataProcessor
 {
     public class DataRetriever
     {
-        public DataRetriever()
+        public DataRetriever(Properties properties)
         {
-            
+            Properties = properties;
+        }
+
+        private Properties Properties{
+            set => _properties = value;
+            get => _properties;
+        }
+        
+        private Properties _properties;
+
+        public List<string> DifferentTracks()
+        {
+            return Properties.UserScores.Select(track => track.Key).ToList();
         }
     }
 }
