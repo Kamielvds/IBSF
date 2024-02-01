60716f632b5fd8ab41ec78dbde8d0f823bfb1c6b
Kamielvds <155197004+Kamielvds@users.noreply.github.com>
2024-02-01 17:39:24 +0100

    Fixed Restructure

--- Commands/DataProcessor/Xml.cs
+++ Commands/DataProcessor/Xml.cs
@@ -7,24 +7,29 @@ using System.Xml.Linq;
 
 namespace Commands.DataProcessor
 {
-    class Xml
+    public class Xml
     {
-        private static Properties _properties;
-        private static string _xmlPath;
+        public Xml(string xmlPath)
+        {
+            SetXmlPath(xmlPath);
+        }
+        
+        private Properties _properties;
+        private string _xmlPath;
 
-        public static string XmlPath
+        public  string XmlPath
         {
             get => _xmlPath;
             set => _xmlPath = value;
         }
 
-        public static Properties Properties
+        public  Properties Properties
         {
             get => _properties;
             set => _properties = value;
         }
 
-        private static void SetXmlPath(string path)
+        private  void SetXmlPath(string path)
         {
             if (File.Exists(path) && path != null) {_xmlPath = path; return;}
             Console.WriteLine("EX: xml-nf");
@@ -33,19 +38,35 @@ namespace Commands.DataProcessor
     }
     public class XmlReader
     {
-        public XmlReader(Properties properties)
+        public XmlReader(Xml xml)
         {
-            Xml.Properties = properties;
+            Xml = xml;
+            
         }
 
+        private Xml _xml;
+        private string _xmlPath;
+
+        public Xml Xml
+        {
+            get => _xml;
+            set => _xml = value;
+        }
+
+        public string XmlPath
+        {
+            get => _xmlPath;
+            set => _xmlPath = value;
+        }
+        
         #region Public Methods
 
         public Dictionary<string, List<Dictionary<string, object>>> LoadXml()
         {
-            if (Xml.XmlPath == null) return null;
+            if (_xmlPath == null) return null;
 
             var xmlDoc = new XmlDocument();
-            xmlDoc.Load(Xml.XmlPath);
+            xmlDoc.Load(_xmlPath);
             var root = xmlDoc.SelectSingleNode("/root");
             var tracksNode = root?.SelectSingleNode("tracks");
             var dictionary = new Dictionary<string, List<Dictionary<string, object>>>();
@@ -105,83 +126,81 @@ namespace Commands.DataProcessor
     }
     public class XmlWriter
     {
-         public XmlWriter(Properties properties)
+        public XmlWriter(Xml xml)
         {
-            SetXmlPath(properties.FilePath);
+            XmlPath = xml.XmlPath;
         }
 
         private string _xmlPath;
 
-        public string XmlPath => _xmlPath;
 
-        #region Private Methods
-
-        private void SetXmlPath(string path)
+        public string XmlPath
         {
-            if (File.Exists(path) && path != null) {_xmlPath = path; return;}
-            Console.WriteLine("EX: xml-nf");
-            _xmlPath = null;
+            set => _xmlPath = value;
         }
 
-        #endregion
-        public void EditElement(string location, string targetNode, string value)
+        /// <summary>
+        /// Changes the Innertext of a given node, when the attribute from the given location is the same
+        /// </summary>
+        /// <param name="location"></param>
+        /// Where to look for the attribute (in root)
+        /// <param name="attribute"></param>
+        /// the name of the attribute
+        /// <param name="attributeValue"></param>
+        /// the value of the attribute on which it is matched
+        /// <param name="targetNode"></param>
+        /// the node inside of the atrribute node that it need to find
+        /// <param name="targetValue"></param>
+        /// the target value for the node
+        public void EditElementFromAttribute(string location, string attribute, string attributeValue,
+            string targetNode, string targetValue)
         {
-            // load
             var xmlDoc = new XmlDocument();
             xmlDoc.Load(_xmlPath);
-            var nodes = xmlDoc.SelectNodes(location);
-            if (nodes == null) return;
-            foreach (XmlNode node in nodes)
+            XmlNode root = xmlDoc.SelectSingleNode("/root");
+            XmlNode targetLocation = root?.SelectSingleNode(location);
+            if (targetLocation == null) return;
+            foreach (var book in targetLocation.Cast<XmlNode>()
+                         .Where(item =>
+                             item.Attributes == null || item.Attributes[attribute].Value == attributeValue))
             {
-                if (node.Name != targetNode) return;
-                node.Value = value;
+                foreach (var item in book.Cast<XmlNode>().Where(subItem => subItem.Name == targetNode))
+                {
+                    item.InnerText = targetValue;
+                    break;
+                }
             }
-        }
-
-        // if you need to rename in different location, use this method in foreach.
-        public void RenameNode(string location, string targetNode, string name)
-        {
-            // load
-            var xmlDoc = new XmlDocument();
-            xmlDoc.Load(_xmlPath);
-            var nodes = xmlDoc.SelectNodes(location);
-            if (nodes == null) return;
-            foreach (XmlNode node in nodes)
-            {
-                if (node.Name != targetNode) return;
-                node.Value = name;
-            }
-        }
-
-        public void AddNodeAndValue(string location, string name, string value)
-        {
-            var xmlDoc = XDocument.Load(_xmlPath);
-            var xElement = new XElement(name, value);
-            xmlDoc.Element(location)?.Add(xElement);
             xmlDoc.Save(_xmlPath);
         }
-
-        public void AddNodeAndValue(string location, string[] names, string[] values)
+        
+        /// <summary>
+        /// Adds a Node, followed by more nodes and InnerText to an xml document
+        /// </summary>
+        /// <param name="location">The location for the node</param>
+        /// <param name="mainNode">The name of the node where to place all the nodes</param>
+        /// <param name="names">All the nodes that should be placed in the mainNode</param>
+        /// <param name="values">All the InnerText to be added to the subnodes</param>
+        public void AddNodeAndValue(string location, string mainNode, List<string> names, List<string> values)
         {
-            if (names == null) throw new ArgumentNullException(nameof(names));
             var xmlDoc = XDocument.Load(_xmlPath);
-            var i = 0;
-            foreach (var name in names)
+            XElement element = xmlDoc.Root?.Element(location);
+            if (element == null)
             {
-                xmlDoc.Element(location)?.Add(new XElement(name, values[i]));
-                i++;
+                element = new XElement(location);
+                xmlDoc.Root?.Add(element);
+            }
+
+            var mainNodeElement = new XElement(mainNode);
+            element.Add(mainNodeElement);
+            for (var i = 0; i < names.Count; i++)
+            {
+                if (names[i] == "id")
+                {
+                    mainNodeElement.Add(new XAttribute("id", values[i]));
+                }else mainNodeElement.Add(new XElement(names[i], values[i]));
             }
 
             xmlDoc.Save(_xmlPath);
-        }
-
-        // used for creating a main node with sub-notes
-        public void AddNodeAndValue(string location, string mainNode, string[] names, string[] values)
-        {
-            var xmlDoc = XDocument.Load(_xmlPath);
-            xmlDoc.Element(location)?.Add(new XElement(mainNode));
-            xmlDoc.Save(_xmlPath);
-            AddNodeAndValue($"{location}/{mainNode}", names, values);
         }
     }
 }
