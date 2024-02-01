d6d1bfd9bfd47e511a1c89c5393e1da805462ce1
Kamielvds <155197004+Kamielvds@users.noreply.github.com>
2024-02-01 18:30:15 +0100

    Added split class

--- Scores/Class1.cs
+++ Scores/Class1.cs
@@ -1,7 +1,51 @@
-﻿namespace Scores
+﻿using System.Collections.Generic;
+
+namespace Scores
 {
     public class Score
     {
         
+    }
+
+    class Split
+    {
+        public Split(List<double> times = null, List<double> distances = null)
+        {
+            Times = times;
+            Distances = distances;
+        }
+        
+        private List<double> _distances;
+        private List<double> _times;
+
+        public List<double> Distances
+        {
+            get => _distances;
+            set => _distances = value;
+        }
+
+        public List<double> Times
+        {
+            get => _times;
+            set => _times = value;
+        }
+
+        public void AddSplit(double time, double distance)
+        {
+            _distances.Add(distance);
+            _times.Add(time);
+        }
+        public void AddSplit(List<double> times, List<double> distances)
+        {
+            foreach (var time in times)
+            {
+                _times.Add(time);
+            }
+
+            foreach (var distance in distances)
+            {
+                _distances.Add(distance);
+            }
+        }
     }
 }
