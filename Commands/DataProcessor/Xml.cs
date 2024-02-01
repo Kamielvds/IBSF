31184d12de3eaea766ed118260de748c6a6582aa
Kamielvds <155197004+Kamielvds@users.noreply.github.com>
2024-02-01 14:51:29 +0100

    Restructure

--- Commands/DataProcessor/Xml.cs
+++ Commands/DataProcessor/Xml.cs
@@ -7,37 +7,45 @@ using System.Xml.Linq;
 
 namespace Commands.DataProcessor
 {
-    public class XmlReader
+    class Xml
     {
-        public XmlReader(Properties properties)
+        private static Properties _properties;
+        private static string _xmlPath;
+
+        public static string XmlPath
         {
-            SetXmlPath(properties.FilePath);
-            properties.UserScores = LoadXml();
+            get => _xmlPath;
+            set => _xmlPath = value;
         }
 
-        private string _xmlPath;
+        public static Properties Properties
+        {
+            get => _properties;
+            set => _properties = value;
+        }
 
-        public string XmlPath => _xmlPath;
-
-        #region Private Methods
-
-        private void SetXmlPath(string path)
+        private static void SetXmlPath(string path)
         {
             if (File.Exists(path) && path != null) {_xmlPath = path; return;}
             Console.WriteLine("EX: xml-nf");
             _xmlPath = null;
         }
-
-        #endregion
+    }
+    public class XmlReader
+    {
+        public XmlReader(Properties properties)
+        {
+            Xml.Properties = properties;
+        }
 
         #region Public Methods
 
         public Dictionary<string, List<Dictionary<string, object>>> LoadXml()
         {
-            if (_xmlPath == null) return null;
+            if (Xml.XmlPath == null) return null;
 
             var xmlDoc = new XmlDocument();
-            xmlDoc.Load(_xmlPath);
+            xmlDoc.Load(Xml.XmlPath);
             var root = xmlDoc.SelectSingleNode("/root");
             var tracksNode = root?.SelectSingleNode("tracks");
             var dictionary = new Dictionary<string, List<Dictionary<string, object>>>();
@@ -94,5 +102,86 @@ namespace Commands.DataProcessor
             return dictionary;
         }
         #endregion
+    }
+    public class XmlWriter
+    {
+         public XmlWriter(Properties properties)
+        {
+            SetXmlPath(properties.FilePath);
+        }
+
+        private string _xmlPath;
+
+        public string XmlPath => _xmlPath;
+
+        #region Private Methods
+
+        private void SetXmlPath(string path)
+        {
+            if (File.Exists(path) && path != null) {_xmlPath = path; return;}
+            Console.WriteLine("EX: xml-nf");
+            _xmlPath = null;
+        }
+
+        #endregion
+        public void EditElement(string location, string targetNode, string value)
+        {
+            // load
+            var xmlDoc = new XmlDocument();
+            xmlDoc.Load(_xmlPath);
+            var nodes = xmlDoc.SelectNodes(location);
+            if (nodes == null) return;
+            foreach (XmlNode node in nodes)
+            {
+                if (node.Name != targetNode) return;
+                node.Value = value;
+            }
+        }
+
+        // if you need to rename in different location, use this method in foreach.
+        public void RenameNode(string location, string targetNode, string name)
+        {
+            // load
+            var xmlDoc = new XmlDocument();
+            xmlDoc.Load(_xmlPath);
+            var nodes = xmlDoc.SelectNodes(location);
+            if (nodes == null) return;
+            foreach (XmlNode node in nodes)
+            {
+                if (node.Name != targetNode) return;
+                node.Value = name;
+            }
+        }
+
+        public void AddNodeAndValue(string location, string name, string value)
+        {
+            var xmlDoc = XDocument.Load(_xmlPath);
+            var xElement = new XElement(name, value);
+            xmlDoc.Element(location)?.Add(xElement);
+            xmlDoc.Save(_xmlPath);
+        }
+
+        public void AddNodeAndValue(string location, string[] names, string[] values)
+        {
+            if (names == null) throw new ArgumentNullException(nameof(names));
+            var xmlDoc = XDocument.Load(_xmlPath);
+            var i = 0;
+            foreach (var name in names)
+            {
+                xmlDoc.Element(location)?.Add(new XElement(name, values[i]));
+                i++;
+            }
+
+            xmlDoc.Save(_xmlPath);
+        }
+
+        // used for creating a main node with sub-notes
+        public void AddNodeAndValue(string location, string mainNode, string[] names, string[] values)
+        {
+            var xmlDoc = XDocument.Load(_xmlPath);
+            xmlDoc.Element(location)?.Add(new XElement(mainNode));
+            xmlDoc.Save(_xmlPath);
+            AddNodeAndValue($"{location}/{mainNode}", names, values);
+        }
     }
 }
