f2c883aaf24fb5c756b2c596596cfb2d651419ca
Kamielvds <155197004+Kamielvds@users.noreply.github.com>
2024-02-05 14:18:50 +0100

    Update Program.cs
    fixed tests

--- Tests/XML/Program.cs
+++ Tests/XML/Program.cs
@@ -13,38 +13,9 @@ namespace Tests
             // creating Obj --> ok
             var properties = new Properties("/Users/kamielvds/Desktop/RiderC#/IBSF-2/Tests/XML/XMLFiles/loading.xml", "xml");
             var xml = new Xml(properties);
-            var xmlProperties = new XmlReader(xml);
-            // retrieving data xml --> ok, but should make a lib => done 
-            /*
-            var item = properties.UserScores.Keys.ElementAt(0);
+            var xmlReader = new XmlReader(xml);
 
-            if (!properties.UserScores.ContainsKey(item)) return;
-            var listOfDictionaries = properties.UserScores[item];
-
-            foreach (var dictionary in listOfDictionaries)
-            {
-                foreach (var kvp in dictionary)
-                {
-                    if (kvp.Key == "splits" && kvp.Value is List<Dictionary<string, string>> list)
-                    {
-                        foreach (var split in list)
-                        {
-                            foreach (var splitKvp in split)
-                            {
-                                Console.WriteLine($"\t {splitKvp.Key}: {splitKvp.Value}");
-                            }
-                            Console.WriteLine("-----");
-                        }
-                    }
-                    else
-                    {
-                        Console.WriteLine($"{kvp.Key}: {kvp.Value}");
-                    }
-                }
-                Console.WriteLine("-----");
-            }
-            */
-            // data retriever -> ok
+            var scores = xmlReader.LoadXml();
         }
     }
 }
