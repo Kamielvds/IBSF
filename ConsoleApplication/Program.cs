e3aeccd7c11b50b12c3a454940dcc6b7138dbb82
Kamielvds <155197004+Kamielvds@users.noreply.github.com>
2024-02-09 09:42:31 +0100

    Update ConsoleApplication

--- ConsoleApplication/Program.cs
+++ ConsoleApplication/Program.cs
@@ -1,9 +1,19 @@
-﻿namespace ConsoleApplication
+﻿using System;
+using System.Collections.Generic;
+using Filtering;
+
+namespace ConsoleApplication
 {
     internal class Program
     {
         public static void Main(string[] args)
         {
+            var list = new List<int> { 12, 435, 456, 5, 768 };
+            Filters.SortDescending(ref list);
+            foreach (var number in list)
+            {
+                Console.WriteLine(number);
+            }
         }
     }
 }
