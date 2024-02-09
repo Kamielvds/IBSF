144b71e9f7c3a44e98fcdd9707df1cad03c7453c
Kamielvds <155197004+Kamielvds@users.noreply.github.com>
2024-02-09 09:41:07 +0100

    Added Activity class

---
+++ ProcessActivity/Class1.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+using Commands;
+using Commands.DataProcessor;
+using ScoreHandeling;
+
+namespace ProcessActivity
+{
+    public class Activitys
+    {
+        public Activitys(string XmlLocation)
+        {
+            Path = XmlLocation;
+            Propeties = new Properties(Path, "xml");
+        }
+
+        private string _path;
+        private Properties _properties;
+        private AllScores _scores = new AllScores();
+        
+
+        public AllScores Scores
+        {
+            get => _scores;
+            set => _scores = value;
+        }
+
+        public string Path
+        {
+            get => _path;
+            set => _path = value;
+        }
+        public Properties Propeties
+        {
+            get => _properties;
+            set => _properties = value;
+        }
+
+        public void LoadAll()
+        {
+            if (_properties == null) return;
+            var xml = new Xml(_properties);
+            var xmlReader = new XmlReader(xml);
+            _scores = xmlReader.LoadScores();
+        }
+        
+        
+    }
+}
