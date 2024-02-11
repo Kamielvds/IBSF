c416ce4f18ca8bfaa6b54e9241583d298202cd13
Kamielvds <155197004+Kamielvds@users.noreply.github.com>
2024-02-11 17:54:16 +0100

    Check test
    Gave error for converting splits time to long, no idea why

--- Tests/XML/Program.cs
+++ Tests/XML/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using Commands;
 using Commands.DataProcessor;
+using ProcessActivity;
+using ScoreHandeling;
 
 namespace Tests
 {
@@ -16,6 +18,10 @@ namespace Tests
             var xmlReader = new XmlReader(xml);
 
             var scores = xmlReader.LoadScores();
+
+            var activitys = new Activitys("/Users/kamielvds/Desktop/RiderC#/IBSF-2/Tests/XML/XMLFiles/loading.xml");
+            activitys.Dispose();
+            activitys.AddScore(new Score());
         }
     }
 }
