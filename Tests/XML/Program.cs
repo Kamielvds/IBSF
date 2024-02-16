a8befb31f6200aa66635cc0a296fa4856fafb7ab
Kamielvds <155197004+Kamielvds@users.noreply.github.com>
2024-02-16 23:57:41 +0100

    Update Program.cs
    Cleaned up tests.

--- Tests/XML/Program.cs
+++ Tests/XML/Program.cs
@@ -12,16 +12,7 @@ namespace Tests
     {
         public static void Main(string[] args)
         {
-            // creating Obj --> ok
-            var properties = new Properties("/Users/kamielvds/Desktop/RiderC#/IBSF-2/Tests/XML/XMLFiles/loading.xml", "xml");
-            var xml = new Xml(properties);
-            var xmlReader = new XmlReader(xml);
-
-            var scores = xmlReader.LoadScores();
-
-            var activitys = new Activitys("/Users/kamielvds/Desktop/RiderC#/IBSF-2/Tests/XML/XMLFiles/loading.xml");
-            activitys.Dispose();
-            activitys.AddScore(new Score());
+            var activitys = new Activitys("/XMLFiles/loading.xml");
         }
     }
 }
