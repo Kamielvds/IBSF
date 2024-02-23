9dfb87441a05c7baa5a31039e0300fd230ba830f
Kamielvds <155197004+Kamielvds@users.noreply.github.com>
2024-02-23 14:02:46 +0100

    Added Errors and Warnings
    Added errors and warnings along with adding default values for the parameters

--- ConsoleApplication/Problems/Errors.cs
+++ ConsoleApplication/Problems/Errors.cs
@@ -10,7 +10,7 @@ namespace ConsoleApplication.Problems
                 Console.WriteLine("There is no file loaded, load one by using the load command");
         }
 
-        public static void InvalidFileType(string filetype)
+        public static void InvalidFileType(string filetype = "null")
         {
             if(UserSettings.ShowErrors)
                 Console.WriteLine($"The file {filetype} is not supported");
@@ -22,10 +22,20 @@ namespace ConsoleApplication.Problems
                 Console.WriteLine("The file could not be found.");
         }
 
-        public static void InvalidInstruction(ushort instruction, int location)
+        public static void InvalidInstruction(byte instruction = 0xFF, int location = 0xFFFFFFF)
         {
             if (UserSettings.ShowErrors)
                 Console.WriteLine($"The instruction {instruction} at value {location} is invalid.");
+        }
+        public static void InstructionInvaid(string command = "null", string instruction = "null")
+        {
+            if (UserSettings.ShowErrors)
+                Console.WriteLine($"the given instruction: {instruction} of the command: {command} does not exist within this context");
+        }
+        public static void ParameterInvaid(string instruction = "null", string parameter = "null")
+        {
+            if (UserSettings.ShowErrors)
+                Console.WriteLine($"the given parameter: {parameter} of the instruction: {instruction} is invalid");
         }
     }
 }
--- ConsoleApplication/Problems/Warnings.cs
+++ ConsoleApplication/Problems/Warnings.cs
@@ -4,40 +4,46 @@ namespace ConsoleApplication.Problems
 {
     public abstract class Warnings
     {
-        public static void CreatedFile(string file)
+        public static void CreatedFile(string file = "null")
         {
             if (UserSettings.ShowWarnings) Console.WriteLine($"The file {file} was not found, so it was created");
         }
 
-        public static void WrongApp(string app)
+        public static void WrongApp(string app = "null")
         {
             if (UserSettings.ShowWarnings) Console.WriteLine($"this file is made for {app}, not XAM");
         }
 
-        public static void SettingNotValid(string setting)
+        public static void SettingNotValid(string setting = "null")
         {
             if (UserSettings.ShowWarnings) Console.WriteLine($"The setting {setting} is not valid, nothing was affected");
         }
 
-        public static void SettingValueNotValid(string value, string setting)
+        public static void SettingValueNotValid(string value = "null", string setting = "null")
         {
             if (UserSettings.ShowWarnings)
                 Console.WriteLine($"The given value {value} for {setting} is not valid, no settings were changed.");
         }
-        public static void FileNotFound(string file)
+        public static void FileNotFound(string file = "null")
         {
             if (UserSettings.ShowWarnings)
                 Console.WriteLine($"the file at path {file} doesn't exists, no changes were made.");
         }
-        public static void DirectoryNotFound(string file)
+        public static void DirectoryNotFound(string file = "null")
         {
             if (UserSettings.ShowWarnings)
                 Console.WriteLine($"the directory at path {file} doesn't exists, no changes were made.");
         }
-        public static void DepricatedMemoryType(string type)
+        public static void DepricatedMemoryType(string type = "null")
         {
             if (UserSettings.ShowWarnings)
                 Console.WriteLine($"the current program is a {type}, which is depricated, please consider using ");
+        }
+
+        public static void CommandNotFound(string command = "null")
+        {
+            if (UserSettings.ShowWarnings)
+                Console.WriteLine($"the command {command} does not exist");
         }
     }
 }
