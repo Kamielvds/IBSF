9f75037e89b9643ca2b91cc6e73203d44585e27b
Kamielvds <155197004+Kamielvds@users.noreply.github.com>
2024-02-26 09:29:05 +0100

    Update Errors.cs
    Added Errors for commands

--- ConsoleApplication/Problems/Errors.cs
+++ ConsoleApplication/Problems/Errors.cs
@@ -6,20 +6,18 @@ namespace ConsoleApplication.Problems
     {
         public static void NoFileLoaded()
         {
-            if(UserSettings.ShowErrors)
+            if (UserSettings.ShowErrors)
                 Console.WriteLine("There is no file loaded, load one by using the load command");
         }
 
         public static void InvalidFileType(string filetype = "null")
         {
-            if(UserSettings.ShowErrors)
-                Console.WriteLine($"The file {filetype} is not supported");
+            if (UserSettings.ShowErrors) Console.WriteLine($"The file {filetype} is not supported");
         }
 
         public static void NoFileFound()
         {
-            if(UserSettings.ShowErrors)
-                Console.WriteLine("The file could not be found.");
+            if (UserSettings.ShowErrors) Console.WriteLine("The file could not be found.");
         }
 
         public static void InvalidInstruction(byte instruction = 0xFF, int location = 0xFFFFFFF)
@@ -27,15 +25,28 @@ namespace ConsoleApplication.Problems
             if (UserSettings.ShowErrors)
                 Console.WriteLine($"The instruction {instruction} at value {location} is invalid.");
         }
-        public static void InstructionInvaid(string command = "null", string instruction = "null")
+
+        public static void InvalidInstruction(string command = "null", string instruction = "null")
         {
             if (UserSettings.ShowErrors)
-                Console.WriteLine($"the given instruction: {instruction} of the command: {command} does not exist within this context");
+                Console.WriteLine(
+                    $"the given instruction: {instruction} of the command: {command} does not exist within this context");
         }
-        public static void ParameterInvaid(string instruction = "null", string parameter = "null")
+
+        public static void InvalidParameter(string instruction = "null", string parameter = "null")
         {
             if (UserSettings.ShowErrors)
                 Console.WriteLine($"the given parameter: {parameter} of the instruction: {instruction} is invalid");
+        }
+
+        public static void InvalidDatatype()
+        {
+            if (UserSettings.ShowErrors) Console.WriteLine("the given data type was incorrect.");
+        }
+
+        public static void NotEnoughArguments()
+        {
+            if (UserSettings.ShowErrors) Console.WriteLine("not enough arguments");
         }
     }
 }
