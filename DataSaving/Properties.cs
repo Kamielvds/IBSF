a7d00a86d8253e884e42b41aa911a8bb1eb81525
Kamielvds <155197004+Kamielvds@users.noreply.github.com>
2024-02-26 09:58:33 +0100

    Update Properties.cs
    Remade the Properties class so the program will be structurally sound again

--- DataSaving/Properties.cs
+++ DataSaving/Properties.cs
@@ -1,10 +1,15 @@
 ﻿using System.Collections.Generic;
+using System.IO;
 
 namespace Commands
 {
-    public class Properties
+    
+    /// <summary>
+    /// Properties class for all readers and writers
+    /// </summary>
+    public abstract class Properties
     {
-        public Properties(string filePath, string lang)
+        protected Properties(string filePath, string lang)
         {
             _filePath = filePath;
             _lang = lang;
@@ -22,6 +27,42 @@ namespace Commands
         {
             get => _lang;
             set => _lang = value;
+        }
+        
+        public bool ValidPath { get; private set; }
+        
+        private bool CheckPath(string path)
+        {
+            if (path == null || !File.Exists(path)) return false;
+            ValidPath = true;
+            return true;
+        }
+
+        public void SetPath(string path)
+        {
+            if (CheckPath(path))
+            {
+                FilePath = path;
+                ValidPath = true;
+            }
+            else
+                ValidPath = false;
+        }
+        
+        /// <summary>
+        /// check if the file exists and path isn't null
+        /// </summary>
+        /// <returns></returns>
+        protected bool CheckPath()
+        {
+            if (FilePath == null || !File.Exists(FilePath))
+            {
+                ValidPath = false;
+                return false;
+            }
+
+            ValidPath = true;
+            return true;
         }
     }
 }
