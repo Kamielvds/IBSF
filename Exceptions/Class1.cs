4b79a64544c282eff29c689dd2c41f9c65921e40
Kamielvds <155197004+Kamielvds@users.noreply.github.com>
2024-02-28 08:41:46 +0100

    Added New Project
    added project for exceptions

---
+++ Exceptions/Class1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Exceptions
+{
+    [Serializable]
+    public class EmptyLocationException : Exception
+    {
+        public EmptyLocationException()
+            : base("The Location is empty.")
+        {
+            HResult = -6675467;
+        }
+        public EmptyLocationException(string s)
+            : base(s)
+        {
+            HResult = -6675467;
+        }
+        public EmptyLocationException(string s,Exception innerException)
+            : base(s,innerException)
+        {
+            HResult = -6675467;
+        }
+        
+    }
+}
