e7b7a71b4601aa85dfc3f2ac662f9b9fe2647737
Kamielvds <155197004+Kamielvds@users.noreply.github.com>
2024-02-28 17:45:09 +0100

    Update Program.cs
    Moved all code to the different classes

--- ConsoleApplication/Program.cs
+++ ConsoleApplication/Program.cs
@@ -2,19 +2,23 @@
 using System.IO;
 using ConsoleApplication.Problems;
 using ProcessActivity;
-using static System.String;
+// static Classes inside project
+using static ConsoleApplication.Settings;
+using static ConsoleApplication.Loading;
+using static ConsoleApplication.Saving;
 
 namespace ConsoleApplication
 {
+    //TODO refactor by using more classes in Project
     internal class Program
     {
         private static bool _running = true;
-        private static string userInput;
-        private static string _xmlPath;
-        private static Activitys _activitys;
-        private static string[] UserInputSplit => userInput.Split(' ');
+        public static string userInput;
+        public static string _path;
+        public static Activitys _activitys;
+        public static string[] UserInputSplit => userInput.Split(' ');
 
-        private static int InputLength => UserInputSplit.Length;
+        public static int InputLength => UserInputSplit.Length;
 
         public static void Main(string[] args)
         {
@@ -23,11 +27,6 @@ namespace ConsoleApplication
             {
                 RequestUserInput();
             }
-        }
-
-        private static void LoadUserSettings()
-        {
-            // TODO make reader 
         }
 
         /// <summary>
@@ -67,95 +66,6 @@ namespace ConsoleApplication
             catch (InvalidDataException)
             {
                 Errors.InvalidDatatype();
-            }
-        }
-
-        /// <summary>
-        /// Loading the sports file, and saving it to _activity's
-        /// </summary>
-        /// <exception cref="InvalidDataException">
-        /// thrown whenever no path is given
-        /// </exception>
-        private static void LoadFile()
-        {
-            string path = null;
-            if (InputLength > 1) path = UserInputSplit[1];
-            if (path == null) throw new InvalidDataException();
-            var lang = Empty;
-            if (File.Exists(path))
-            {
-                _activitys = InputLength > 2 ? new Activitys(path, UserInputSplit[2]) : new Activitys(path);
-            }
-            else
-                Warnings.FileNotFound(path);
-        }
-
-        private static void SaveFile()
-        {
-            if (_xmlPath == null)
-            {
-                Errors.NoFileLoaded();
-                return;
-            }
-
-            _activitys.SaveFile();
-        }
-
-        private static void ProcessSettingCommand()
-        {
-            string task = UserInputSplit[1];
-            switch (task)
-            {
-                case "edit":
-                case "-e":
-                    EditSetting();
-                    break;
-                case "list":
-                case "-l":
-                    ListSetting();
-                    break;
-            }
-        }
-
-        private static void ListSetting()
-        {
-            var setting = UserInputSplit[2];
-            switch (setting)
-            {
-                case "*":
-                case "all":
-                    foreach (var kvp in UserSettings.Settings)
-                    {
-                        Console.WriteLine($"{kvp.Key}: {kvp.Value}");
-                    }
-
-                    break;
-                default:
-                    Console.WriteLine(UserSettings.Settings[setting]);
-                    break;
-            }
-        }
-
-        private static void EditSetting()
-        {
-            var setting = UserInputSplit[2];
-            switch (setting)
-            {
-                case "ShowWarnings":
-                    UserSettings.ShowWarnings = Convert.ToBoolean(UserInputSplit[3]);
-                    break;
-                case "ShowErrors":
-                    UserSettings.ShowErrors = Convert.ToBoolean(UserInputSplit[3]);
-                    break;
-                case "LinesBeforeUser":
-                    UserSettings.LinesBeforeUser = Convert.ToInt32(UserInputSplit[3]);
-                    break;
-                case "LinesAfterUser":
-                    UserSettings.LinesBeforeUser = Convert.ToInt32(UserInputSplit[3]);
-                    break;
-                default:
-                    Warnings.SettingNotValid(setting);
-                    break;
             }
         }
     }
