29d4443460dc8eee27168a91252f8dd105935334
Kamielvds <155197004+Kamielvds@users.noreply.github.com>
2024-03-02 15:56:41 +0100

    Update Score.cs
    Added a listing feature based on location, and the ability to add scores.

--- ConsoleApplication/Score.cs
+++ ConsoleApplication/Score.cs
@@ -25,8 +25,72 @@ namespace ConsoleApplication
                 case "-l":
                     ListScores();
                     break;
+                case "add":
+                    AddScore();
+                    break;
                 default:
                     throw new InvalidArguments($"score:{UserInputSplit[1]}");
+            }
+        }
+
+        /// <summary>
+        /// Add a score to the Activity's local score system, !!! not saved to file !!!
+        /// </summary>
+        private static void AddScore()
+        {
+            Console.WriteLine("location name:");
+            var location = Console.ReadLine();
+            
+            // score
+            Console.WriteLine("name:");
+            var name = Console.ReadLine();
+            Console.WriteLine("age:");
+            var age = Console.ReadLine();
+            Console.WriteLine("note:");
+            var note = Console.ReadLine();
+            Console.WriteLine("gender:");
+            var gender = Console.ReadLine();
+            Console.WriteLine("nationality:");
+            var nationality = Console.ReadLine();
+            Console.WriteLine("date:");
+            var date = Console.ReadLine();
+            Console.WriteLine("submitted:");
+            var submitted = Console.ReadLine();
+
+            var times       = new List<long>();
+            var distances   = new List<double>();
+            
+            Console.WriteLine("How many splits?");
+            for (int i = Convert.ToInt32(Console.ReadLine()); i > 0 ; i--)
+            {
+                
+                Console.WriteLine("time:");
+                times.Add(Convert.ToInt32(Console.ReadLine()));
+                Console.WriteLine("distance:");
+                distances.Add(Convert.ToDouble(Console.ReadLine()));
+            }
+            
+            
+            
+            // added last so if a convert issue happens it isn't all bad.
+            try
+            {
+                Activitys.CreateSplit(times, distances);
+                if (gender != null)
+                    Activitys.CreateScore(name, Convert.ToInt32(age), nationality, Convert.ToBoolean(submitted),
+                        Convert.ToDateTime(date), Convert.ToChar(gender), note);
+                else throw new NullReferenceException();
+                Activitys.CreateLocation(location);
+            }
+            catch (NullReferenceException)
+            {
+                Console.WriteLine("the gender was null.");
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("the score coudn't be created because one of the parameters was wrong. " +
+                                  "Please check all the arguments and try again.");
+                throw;
             }
         }
 
@@ -44,9 +108,25 @@ namespace ConsoleApplication
                 case "*":
                     ListAllScores();
                     break;
+                case "location":
+                    ListAllLocations();
+                    break;
                 case "in":
                     ListFrom();
                     break;
+            }
+        }
+
+        /// <summary>
+        /// List all the different scores.
+        /// </summary>
+        private static void ListAllLocations()
+        {
+            Console.WriteLine("id:");   // used as indicator
+            for (var i = 0; i < AllScores.Locations.Count; i++)
+            {
+                var location = AllScores.Locations[i];
+                Console.WriteLine($" {i}: {location.Name}");
             }
         }
 
@@ -55,7 +135,60 @@ namespace ConsoleApplication
         /// </summary>
         private static void ListFrom()
         {
-            // TODO
+            if (UserInputSplit.Length < 4) throw new NotEnoughArguments();
+            switch (UserInputSplit[3])
+            {
+                case "location":
+                    ListLocation();
+                    break;
+            }
+        }
+
+        private static void ListLocation()
+        {
+            if (UserInputSplit.Length < 5) throw new NotEnoughArguments();
+            try
+            {
+                foreach (var location in AllScores.Locations)
+                {
+                    if (location.Name  != UserInputSplit[4]) continue;
+                    for (var j = 0; j < location.Scores.Count; j++)
+                    {
+                        var score = location.Scores[j];
+                        Console.WriteLine($"Activity {j + 1}:");
+                        foreach (var item in score.AllObjects)
+                        {
+                            switch (item.Value)
+                            {
+                                case null:
+                                    Console.WriteLine($"\t {item.Key}:");
+                                    break;
+                                case string _:
+                                    Console.WriteLine($"\t {item.Key}: {item.Value}");
+                                    break;
+                                default:
+                                    Console.WriteLine($"\t splits:");
+                                    for (var i = 0; i < ((List<Score.Split>)item.Value).Count; i++)
+                                    {
+                                        var split = ((List<Score.Split>)item.Value)[i];
+                                        Console.WriteLine($"\t \t split {i + 1}:");
+                                        Console.WriteLine($"\t \t \t distance: {split.Distance}:");
+                                        Console.WriteLine($"\t \t \t time: {split.Time + 1}:");
+                                    }
+
+                                    break;
+                            }
+                        }
+                    }
+                    return;
+                }
+                Console.WriteLine("Score was not found.");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
         }
 
         /// <summary>
