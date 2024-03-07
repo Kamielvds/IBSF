f0ac54acc895dd4148d088710476cc4c4d64a4a8
Kamielvds <155197004+Kamielvds@users.noreply.github.com>
2024-03-07 17:46:24 +0100

    Update Settings.cs

--- ConsoleApplication/Settings.cs
+++ ConsoleApplication/Settings.cs
@@ -1,5 +1,7 @@
 using System;
 using ConsoleApplication.Problems;
+// static
+using static ConsoleApplication.CustomMethods;
 
 namespace ConsoleApplication
 {
@@ -82,28 +84,6 @@ namespace ConsoleApplication
                 default:
                     Warnings.SettingNotValid(setting);
                     break;
-            }
-        }
-
-        /// <summary>
-        /// support for 0-1 as a valid "falsy" or "truey" type. since default convert doesn't 
-        /// </summary>
-        /// <param name="s">
-        /// the string that needs to be converted
-        /// </param>
-        /// <returns>
-        /// wheather the value is true or false.
-        /// </returns>
-        private static bool CheckBoolean(string s)
-        {
-            switch (s)
-            {
-                case "0":
-                    return false;
-                case "1":
-                    return true;
-                default:
-                    return Convert.ToBoolean(s);
             }
         }
     }
